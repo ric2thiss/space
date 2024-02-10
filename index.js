8713edfdfda927ba5e0669383001876d576986f9
ricpaquibot <ric.paquibot@vikingassetmanagement.com>
2024-02-11 01:16:15 +0800

    correction in menu nav links - worked!

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const toggle_menu = document.querySelector('#toggle_menu')
 const primaryNav = document.querySelector('.primary_nav')
+
+toggle_menu.addEventListener("click",()=>{
+    const visibility = primaryNav.getAttribute('data-visible');
+    if(visibility === "false") {
+        primaryNav.setAttribute('data-visible',true)
+    }else{
+        primaryNav.setAttribute('data-visible', false)
+    }
+
+
+    console.log(visibility)
+})
