eae5cbe3b39ec9150ede28774820a5508c2b3e41
ricpaquibot <ric.paquibot@vikingassetmanagement.com>
2024-02-13 03:11:05 +0800

    added close button in toggle menu

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ toggle_menu.addEventListener("click",()=>{
     const visibility = primaryNav.getAttribute('data-visible');
     if(visibility === "false") {
         primaryNav.setAttribute('data-visible',true)
+        toggle_menu.style.backgroundImage = 'url(./assets/shared/icon-close.svg)';
     }else{
         primaryNav.setAttribute('data-visible', false)
+        toggle_menu.style.backgroundImage = 'url(./assets/shared/icon-hamburger.svg)';
     }
 
 
